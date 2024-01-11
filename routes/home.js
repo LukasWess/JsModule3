ed2855fd4e4bba2e7f746464ffc59235f45bd308
Lukas Wessels <Lukas25@live.no>
2024-01-11 19:40:11 +0100

    Refactor typed.js code

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+const { data } = require('jquery');
+const path = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home');
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  let array = JSON.parse(data);
+  res.render('home', { array: JSON.parse(data)});
+  console.log(array);
 });
-
+console.log(data);
 module.exports = router;
