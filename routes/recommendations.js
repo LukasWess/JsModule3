d716d15111b74ed04e6f51c204331791d913ed3e
Lukas Wessels <Lukas25@live.no>
2024-01-14 00:48:20 +0100

    minor tweeks

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -4,48 +4,56 @@ const fs = require("fs");
 const path = require("path");
 var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
+var ensureLogIn = require("connect-ensure-login").ensureLoggedIn;
+var ensureLoggedIn = ensureLogIn();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let data = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
-  res.render('recommendations', { data: JSON.parse(data)
- });
+router.get("/", function (req, res, next) {
+  let data = fs.readFileSync(
+    path.resolve(__dirname, "../data/recommendations.json")
+  );
+  res.render("recommendations", { data: JSON.parse(data) });
 });
-
-router.post('/', jsonParser, function(req, res, next) {
-  const expectedAttributed = ["url", "name", "alt", "category", "header", "description"]
-    Object.keys(req.body).forEach(param => {
-        if (!(expectedAttributed.includes(param))) {
-            res.status(400).end("Specify only the expected attributes");
-        }
-        else{
-          if(req.body[param] == ''){
-            res.status(400).end(param + " must have a value");
-          }
-        }
-    });
-    if (req.body.url == null || req.body.name == null) {
-      res.status(400).end("Url/name not provided");
-    }
-    if (req.body.category != null) {
-      if (!(["wedding", "christmas", "birthday", "anniversary"].includes(req.body.category))) {
-         res.status(400).end("Wrong category provided");
+//Post New Recommendations
+router.post("/", jsonParser, function (req, res, next) {
+  const expectedAttributed = ["avatar", "name", "role", "description"];
+  Object.keys(req.body).forEach((param) => {
+    if (!expectedAttributed.includes(param)) {
+      console.log(param);
+      res.status(400).end("Wrong Attr");
+    } else {
+      if (req.body[param] == "") {
+        res.status(400).end(param + " must have a value");
       }
     }
+  });
+  if (req.body.avatar == null || req.body.name == null) {
+    res.status(400).end("Avatar/name not provided");
+  }
+  if (![1, 2, 3].includes(req.body.avatar)) {
+    res.status(400).end("Wrong avatar provided");
+  }
 
-  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
-  let portfoliosArray = JSON.parse(rawdata);
-  if(portfoliosArray.filter(x => x.name === req.body.name).length == 0) {
-    download(req.body.url, req.body.name, function(){
-      console.log('done');
-    });
-    const newArray = portfoliosArray.concat([req.body])
-    fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(newArray));
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../data/recommendations.json")
+  );
+  let recommendationsArray = JSON.parse(rawdata);
+  if (
+    recommendationsArray.filter((x) => x.name === req.body.name).length == 0
+  ) {
+    const newArray = recommendationsArray.concat([req.body]);
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/recommendations.json"),
+      JSON.stringify(newArray)
+    );
   }
   res.end();
 });
-
-router.delete("/", jsonParser, function (req, res, next) {
+//Delete Recommendations
+router.delete("/", jsonParser, ensureLogIn, function (req, res, next) {
+  if (req.body.name == null) {
+    res.status(400).end("Name not provided");
+  }
   let rawdata = fs.readFileSync(
     path.resolve(__dirname, "../data/recommendations.json")
   );
