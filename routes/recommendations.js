4bbb16f6c1862df54ab712dd403a3d4aea8cace0
Lukas Wessels <Lukas25@live.no>
2024-01-13 22:27:57 +0100

    Add POST and DELETE recommendations handlers

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -1,13 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+router.get("/", function (req, res, next) {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../data/recommendations.json")
+  );
   let recommendations = JSON.parse(rawdata);
-  res.render('recommendations', { recommendations: recommendations });
+  res.render("recommendations", { recommendations: recommendations });
+});
+
+router.post("/", jsonParser, function (req, res, next) {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../data/recommendations.json")
+  );
+  let recommendationsArray = JSON.parse(rawdata);
+  if (
+    recommendationsArray.filter((x) => x.name === req.body.name).length == 0
+  ) {
+    const newArray = recommendationsArray.concat([req.body]);
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/recommendations.json"),
+      JSON.stringify(newArray)
+    );
+  }
+  res.end();
+});
+
+router.delete("/", jsonParser, function (req, res, next) {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../data/recommendations.json")
+  );
+  let recommendationsArray = JSON.parse(rawdata);
+  const newArray = recommendationsArray.filter((x) => x.name !== req.body.name);
+  if (newArray.length !== recommendationsArray.length) {
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/recommendations.json"),
+      JSON.stringify(newArray)
+    );
+  }
+  res.end();
 });
 
 module.exports = router;
